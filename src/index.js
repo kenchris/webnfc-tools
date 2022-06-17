6948d620ecbfe664c2db668016b57162cbfb9d67
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2022-06-17 14:55:32 +0200

    WIP

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-import { LitElement, html, css } from "../web_modules/lit-element.js";
-import "../web_modules/@material/mwc-drawer.js";
-import "../web_modules/@material/mwc-top-app-bar.js";
-import "../web_modules/@material/mwc-icon-button.js";
-import "../web_modules/@material/mwc-button.js";
-import "../web_modules/@material/mwc-dialog.js";
-import "../web_modules/@material/mwc-snackbar.js";
-import "../web_modules/@material/mwc-select.js";
-import '../web_modules/@material/mwc-menu.js';
-import '../web_modules/@material/mwc-textfield.js';
-import '../web_modules/@material/mwc-list/mwc-list-item.js';
+import { LitElement, html, css } from 'https://unpkg.com/lit-element?module';
 
 class NDEFRecordRoot extends LitElement {
   static styles = css`
@@ -25,7 +15,8 @@ class NDEFRecordRoot extends LitElement {
     }
 
     #header {
-      width: calc(100% + 16px);
+      margin-left: 12px;
+      width: calc(100% + 12px);
       height: 42px;
       display: flex;
       justify-content: flex-start;
@@ -65,17 +56,43 @@ class NDEFRecordRoot extends LitElement {
         &nbsp;&nbsp;&nbsp;
         <div id="expand"></div>
         <div style="position: relative;">
-          <mwc-icon-button id="menu-button" icon="add" @click=${() => this.menu.show()}></mwc-icon-button>
-          <mwc-menu id="menu">
-            <mwc-list-item @click=${() => addRecord("record-empty")}>Empty record</mwc-list-item>
-            <mwc-list-item @click=${() => addRecord("record-text")}>Text record</mwc-list-item>
-            <mwc-list-item @click=${() => addRecord("record-url")}>URL record</mwc-list-item>
-            <mwc-list-item @click=${() => addRecord("record-absolute-url")}>Absolute URL record</mwc-list-item>
-            <mwc-list-item @click=${() => addRecord("record-empty")}>Smart Poster record</mwc-list-item>
-            <mwc-list-item @click=${() => addRecord("record-mime")}>MIME type record</mwc-list-item>
-            <mwc-list-item @click=${() => addRecord("record-empty")}>External type record</mwc-list-item>
-            <mwc-list-item @click=${() => addRecord("record-empty")}>Unknown record</mwc-list-item>
-          </mwc-menu>
+          <sl-dropdown>
+            <sl-icon-button id="menu-button" slot="trigger" name="plus-square-dotted" style="font-size: 1.5rem;"></sl-icon-button>
+            <sl-menu>
+              <sl-menu-item @click=${() => addRecord("record-empty")}>
+                Empty record
+                <sl-icon slot="prefix" name="chat-right"></sl-icon>
+              </sl-menu-item>
+              <sl-menu-item @click=${() => addRecord("record-text")}>
+                Text record
+                <sl-icon slot="prefix" name="chat-right-text"></sl-icon>
+              </sl-menu-item>
+              <sl-menu-item @click=${() => addRecord("record-url")}>
+                URL record
+                <sl-icon slot="prefix" name="house"></sl-icon>
+              </sl-menu-item>
+              <sl-menu-item @click=${() => addRecord("record-absolute-url")}>
+                Absolute URL record
+                <sl-icon slot="prefix" name="house"></sl-icon>
+              </sl-menu-item>
+              <sl-menu-item @click=${() => addRecord("record-empty")}>
+                Smart Poster record
+                <sl-icon slot="prefix" name="easel2"></sl-icon>
+              </sl-menu-item>
+              <sl-menu-item @click=${() => addRecord("record-mime")}>
+                MIME type record
+                <sl-icon slot="prefix" name="file-bar-graph"></sl-icon>
+              </sl-menu-item>
+              <sl-menu-item @click=${() => addRecord("record-external")}>
+                External type record
+                <sl-icon slot="prefix" name="box-arrow-up-right"></sl-icon>
+              </sl-menu-item>
+              <sl-menu-item @click=${() => addRecord("record-empty")}>
+                Unknown record
+                <sl-icon slot="prefix" name="patch-question"></sl-icon>
+              </sl-menu-item>
+            </sl-menu>
+          </sl-dropdown>
         </div>
       </div>
       <slot></slot>
@@ -93,7 +110,7 @@ const subItemStyle = css`
   }
 
   #header {
-    width: calc(100% + 16px);
+    width: calc(100% + 12px);
     height: 18px;
     display: flex;
     justify-content: flex-start;
@@ -110,7 +127,7 @@ const subItemStyle = css`
     flex: 1;
   }
 
-  mwc-textfield, mwc-select {
+  sl-input, sl-select, #content > * {
     margin: 16px 0px 0px 0px;
     width: 100%;
   }
@@ -133,7 +150,7 @@ class NDEFEmptyRecord extends LitElement {
         <span>Empty</span>
         <div id="expand"></div>
         <div style="position: relative;">
-          <mwc-icon-button icon="remove" @click=${() => this.remove()}></mwc-icon-button>
+          <sl-icon-button name="dash-square-dotted" style="font-size: 1.5rem;" @click=${() => this.remove()}></sl-icon-button>
         </div>
       </div>
     `;
@@ -142,12 +159,7 @@ class NDEFEmptyRecord extends LitElement {
 customElements.define("record-empty", NDEFEmptyRecord);
 
 class NDEFTextRecord extends LitElement {
-  static styles = [subItemStyle, css`
-    mwc-textfield {
-      margin: 16px 0px 0px 0px;
-      width: 100%;
-    }
-  `];
+  static styles = [subItemStyle];
 
   records() {
     const textValue = this.shadowRoot.querySelector("#text-input").value || "";
@@ -170,27 +182,22 @@ class NDEFTextRecord extends LitElement {
         <span>Text (UTF-8)</span>
         <div id="expand"></div>
         <div style="position: relative;">
-          <mwc-icon-button icon="remove" @click=${() => this.remove()}></mwc-icon-button>
+          <sl-icon-button name="dash-square-dotted" style="font-size: 1.5rem;" @click=${() => this.remove()}></sl-icon-button>
         </div>
       </div>
-      <mwc-textfield id="text-input" outlined label="Text"></mwc-textfield>
-      <mwc-textfield id="lang-input" outlined label="Language code" value="en-US"></mwc-textfield>
-      <mwc-select id="encoding-input" outlined label="Encoding">
-        <mwc-list-item value="utf-8" selected>UTF-8</mwc-list-item>
-        <mwc-list-item value="utf-16">UTF-16</mwc-list-item>
-      </mwc-select>
+      <sl-input id="text-input" label="Text" clearable></sl-input>
+      <sl-input id="lang-input" label="Language code" placeholder="ISO language code" value="en-US" clearable></sl-input>
+      <sl-select id="encoding-input" label="Encoding" value="utf-8">
+        <sl-menu-item value="utf-8">UTF-8</sl-menu-item>
+        <sl-menu-item value="utf-16">UTF-16</sl-menu-item>
+      </sl-select>
     `;
   }
 }
 customElements.define("record-text", NDEFTextRecord);
 
 class NDEFMIMERecord extends LitElement {
-  static styles = [subItemStyle, css`
-    mwc-textfield, mwc-button, #data-size {
-      margin: 16px 0px 0px 0px;
-      width: 100%;
-    }
-  `];
+  static styles = [subItemStyle];
 
   constructor() {
     super();
@@ -246,16 +253,139 @@ class NDEFMIMERecord extends LitElement {
         <span>Text (UTF-8)</span>
         <div id="expand"></div>
         <div style="position: relative;">
-          <mwc-icon-button icon="remove" @click=${() => this.remove()}></mwc-icon-button>
+          <sl-icon-button name="dash-square-dotted" style="font-size: 1.5rem;" @click=${() => this.remove()}></sl-icon-button>
         </div>
       </div>
-      <mwc-textfield id="mime-input" outlined label="MIME type" value="application/octet-stream"></mwc-textfield>
-      <div id="data-size">Data source size: <span id="size">0</span></div>
-      <mwc-button @click=${() => this.selectDataSource()}>Select data source</mwc-button>
+      <div id="content">
+        <sl-input id="mime-input" label="MIME type" value="application/octet-stream" clearable></sl-input>
+        <sl-button name="file" style="font-size: 1.5rem;" @click=${() => this.selectDataSource()}>
+          Select data source (current size <span id="size">0</span> bytes)
+        </sl-icon-button>
+      </div>
     `;
   }
 }
 customElements.define("record-mime", NDEFMIMERecord);
+
+class NDEFExternalRecord extends LitElement {
+  static styles = [subItemStyle,
+    css`
+    :host {
+      border: solid;
+      margin: 16px;
+      padding: 6px 16px 6px 16px;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+      align-content: stretch;
+    }
+
+    #header {
+      margin-left: 12px;
+      width: calc(100% + 12px);
+      height: 42px;
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      align-content: stretch;
+    }
+
+    #header #expand {
+      flex: 1;
+    }
+  `];
+  
+  records() {
+    const namespaceValue = this.shadowRoot.querySelector("#ns-input").value || "example.com";
+    const nameValue = this.shadowRoot.querySelector("#name-input").value || "myType";
+
+    let records = [];
+    for (let child of Array.from(this.children)) {
+      if ("records" in child) {
+        records.push(child.records());
+      }
+    }
+
+    
+    return {
+      recordType: `${namespaceValue}:${nameValue}`,
+      data: records
+    }
+  }
+
+  render() {
+    const addRecord = (name) => {
+      const element = document.createElement(name);
+      this.appendChild(element);
+    }
+
+    return html`
+      <div id="header">
+        Record
+        &nbsp;&nbsp;&nbsp;
+        <span>External</span>
+        <div id="expand"></div>
+        <div style="position: relative;">
+          <sl-icon-button name="dash-square-dotted" style="font-size: 1.5rem;" @click=${() => this.remove()}></sl-icon-button>
+        </div>
+      </div>
+      <sl-input id="ns-input" label="Namespace" placeholder="example.com" clearable></sl-input>
+      <sl-input id="name-input" label="Name" placeholder="myType" clearable></sl-input>
+
+      <br>
+      <slot></slot>
+      <br>
+
+      <div id="header">
+        <sl-dropdown>
+          <sl-button variant="default" slot="trigger" >
+           <sl-icon slot="prefix" name="plus-square-dotted"></sl-icon>
+            Add subrecord
+          </sl-button>
+          <sl-menu>
+            <sl-menu-item @click=${() => addRecord("record-empty")}>
+              Empty record
+              <sl-icon slot="prefix" name="chat-right"></sl-icon>
+            </sl-menu-item>
+            <sl-menu-item @click=${() => addRecord("record-text")}>
+              Text record
+              <sl-icon slot="prefix" name="chat-right-text"></sl-icon>
+            </sl-menu-item>
+            <sl-menu-item @click=${() => addRecord("record-url")}>
+              URL record
+              <sl-icon slot="prefix" name="house"></sl-icon>
+            </sl-menu-item>
+            <sl-menu-item @click=${() => addRecord("record-absolute-url")}>
+              Absolute URL record
+              <sl-icon slot="prefix" name="house"></sl-icon>
+            </sl-menu-item>
+            <sl-menu-item @click=${() => addRecord("record-empty")}>
+              Smart Poster record
+              <sl-icon slot="prefix" name="easel2"></sl-icon>
+            </sl-menu-item>
+            <sl-menu-item @click=${() => addRecord("record-mime")}>
+              MIME type record
+              <sl-icon slot="prefix" name="file-bar-graph"></sl-icon>
+            </sl-menu-item>
+            <sl-menu-item @click=${() => addRecord("record-external")}>
+              External type record
+              <sl-icon slot="prefix" name="box-arrow-up-right"></sl-icon>
+            </sl-menu-item>
+            <sl-menu-item @click=${() => addRecord("record-empty")}>
+              Unknown record
+              <sl-icon slot="prefix" name="patch-question"></sl-icon>
+            </sl-menu-item>
+          </sl-menu>
+        </sl-dropdown>
+      </div>
+    </div>
+    `;
+  }
+}
+customElements.define("record-external", NDEFExternalRecord);
+
+
 
 class NDEFURLRecord extends LitElement {
   static styles = [subItemStyle];
@@ -277,10 +407,10 @@ class NDEFURLRecord extends LitElement {
         <span>URL</span>
         <div id="expand"></div>
         <div style="position: relative;">
-          <mwc-icon-button icon="remove" @click=${() => this.remove()}></mwc-icon-button>
+          <sl-icon-button name="dash-square-dotted" style="font-size: 1.5rem;" @click=${() => this.remove()}></sl-icon-button>
         </div>
       </div>
-      <mwc-textfield id="text-input" outlined type="url" label="URL"></mwc-textfield>
+      <sl-input id="text-input" label="URL" placeholder="https://" clearable></sl-input>
     `;
   }
 }
@@ -306,10 +436,10 @@ class NDEFAbsoluteURLRecord extends LitElement {
         <span>Absolute URL</span>
         <div id="expand"></div>
         <div style="position: relative;">
-          <mwc-icon-button icon="remove" @click=${() => this.remove()}></mwc-icon-button>
+          <sl-icon-button name="dash-square-dotted" style="font-size: 1.5rem;" @click=${() => this.remove()}></sl-icon-button>
         </div>
       </div>
-      <mwc-textfield id="text-input" outlined type="url" label="URL"></mwc-textfield>
+      <sl-input id="text-input" label="URL" placeholder="https://" clearable></sl-input>
     `;
   }
 }
@@ -336,11 +466,6 @@ class NFCTools extends LitElement {
   `;
 
   async firstUpdated() {
-    const drawer = this.shadowRoot.querySelector("mwc-drawer");
-    const container = drawer.parentNode;
-    container.addEventListener('MDCTopAppBar:nav', _ => {
-      drawer.open = !drawer.open;
-    });
     this.rootRecord = this.shadowRoot.querySelector("record-root");
   }
 
@@ -356,29 +481,12 @@ class NFCTools extends LitElement {
 
   render() {
     return html`
-      <mwc-drawer hasHeader type=modal>
-        <span slot="title">Web NFC tools</span>
-        <span slot="subtitle">Configure your NFC tags today</span>
-        <div class="drawer-content">
-          <p><a href="https://w3c.github.io/web-nfc/">Web NFC API specification</a></p>
-        </div>
-        <div slot="appContent">
-          <mwc-top-app-bar>
-            <mwc-icon-button slot="navigationIcon" icon="menu"></mwc-icon-button>
-            <div slot="title">Web NFC tools</div>
-          </mwc-top-app-bar>
-          <div class="main-content">
-            <record-root>
-              <record-text></record-text>
-            </record-root>
-            <mwc-button @click=${() => this.write()}>Write</mwc-button>
-          </div>
-        </div>
-      </mwc-drawer>
-      <mwc-snackbar id="snackbar">
-        <mwc-button id="actionButton" slot="action">CANCEL</mwc-button>
-        <mwc-icon-button id="iconButton" icon="close" slot="dismiss"></mwc-icon-button>
-      </mwc-snackbar>
+      <div class="main-content">
+        <record-root>
+          <record-text></record-text>
+        </record-root>
+        <sl-button variant="primary" @click=${() => this.write()}>Write</sl-button>
+      </div>
     `;
   }
 }
