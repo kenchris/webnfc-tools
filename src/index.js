3f412f6bbe21ddac13aca2fffc132abde97180e3
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2022-06-21 14:45:15 +0200

    Add a bit of form validation

--- src/index.js
+++ src/index.js
@@ -29,11 +29,6 @@ class NDEFRecordRoot extends LitElement {
     }
   `;
 
-  firstUpdated() {
-    this.menu = this.shadowRoot.querySelector("#menu");
-    this.menu.anchor = this.shadowRoot.querySelector("#menu-button");
-  }
-
   records() {
     let records = [];
     for (let child of Array.from(this.children)) {
@@ -286,7 +281,7 @@ class NDEFExternalRecord extends LitElement {
       width: calc(100% + 12px);
       height: 42px;
       display: flex;
-      justify-content: flex-start;
+      justify-content: center;
       align-items: center;
       align-content: stretch;
     }
@@ -294,11 +289,34 @@ class NDEFExternalRecord extends LitElement {
     #header #expand {
       flex: 1;
     }
+
+    sl-dropdown {
+      margin: 4px 0 16px 0;
+    }
+
+    sl-input[invalid]:not([disabled])::part(label),
+    sl-input[invalid]:not([disabled])::part(help-text) {
+      color: var(--sl-color-danger-600);
+    }
+
+    sl-input[invalid]:not([disabled])::part(base) {
+      border-color: var(--sl-color-danger-500);
+    }
+
+    sl-input[invalid]:focus-within::part(base) {
+      box-shadow: 0 0 0 var(--sl-focus-ring-width) var(--sl-color-danger-500);
+    }
   `];
   
   records() {
-    const namespaceValue = this.shadowRoot.querySelector("#ns-input").value || "example.com";
-    const nameValue = this.shadowRoot.querySelector("#name-input").value || "myType";
+    const typeInput = this.shadowRoot.querySelector("#type-input");
+    this.validateExternalType(typeInput.value, typeInput);
+
+    const isValid = typeInput.reportValidity();
+
+    if (!isValid) {
+      throw Error;
+    }
 
     let records = [];
     for (let child of Array.from(this.children)) {
@@ -307,11 +325,52 @@ class NDEFExternalRecord extends LitElement {
       }
     }
 
-    
     return {
-      recordType: `${namespaceValue}:${nameValue}`,
+      recordType: typeInput.value,
       data: records
     }
+  }
+
+  validateExternalType() {
+    // https://w3c.github.io/web-nfc/#dfn-validate-external-type
+  
+    const input = this.shadowRoot.querySelector("#type-input");
+    const str = input.value;
+
+    if (str.length == 0 || str.length > 255) {
+      return input.setCustomValidity("External type has to be between 0 and 255 characters long.");
+    }
+  
+    const isAscii = /^[\u0000-\u007f]*$/.test(str);
+    if (!isAscii) {
+      return input.setCustomValidity("External type may only contain ASCII characters.");
+    }
+
+    const parts = str.split(":");
+    if (parts.length < 2) {
+      return input.setCustomValidity("External type must include a ':' character.");
+    }
+    if (parts.length > 2) {
+      return input.setCustomValidity("External type may only include one ':' character.");
+    }
+
+    let [domain, type] = parts;
+
+    if (domain.length == 0) {
+      return input.setCustomValidity("Domain part may not be empty.");
+    }
+
+    const isInvalidDomain = /^[\u0000|\u0009|\u000a|\u000d|\u0020|\u0023|\u002f|\u003a|\u003c|\u003e|\u003f|\u0040|\u005b|\u005c|\u005d|\u005e|\u007c|\u005f]*$/.test(domain);
+    if (isInvalidDomain) {
+      return input.setCustomValidity("Domain part contains illegal characters.");
+    }
+
+    const isValidType = /^[1-9A-Za-z$'()\*\+,\-\.;=$@_]*$/.test(type)
+    if (!isValidType) {
+      return input.setCustomValidity("Type part contains illegal characters.");
+    }
+
+    return input.setCustomValidity("");
   }
 
   render() {
@@ -330,55 +389,52 @@ class NDEFExternalRecord extends LitElement {
           <sl-icon-button name="dash-square-dotted" style="font-size: 1.5rem;" @click=${() => this.remove()}></sl-icon-button>
         </div>
       </div>
-      <sl-input id="ns-input" label="Namespace" placeholder="example.com" clearable></sl-input>
-      <sl-input id="name-input" label="Name" placeholder="myType" clearable></sl-input>
+      <sl-input id="type-input" label="Custom type with mandatory namespace" placeholder="example.com:myType" @sl-input=${() => this.validateExternalType()} required clearable></sl-input>
 
       <br>
       <slot></slot>
       <br>
 
-      <div id="header">
-        <sl-dropdown>
-          <sl-button variant="default" slot="trigger" >
-           <sl-icon slot="prefix" name="plus-square-dotted"></sl-icon>
-            Add subrecord
-          </sl-button>
-          <sl-menu>
-            <sl-menu-item @click=${() => addRecord("record-empty")}>
-              Empty record
-              <sl-icon slot="prefix" name="chat-right"></sl-icon>
-            </sl-menu-item>
-            <sl-menu-item @click=${() => addRecord("record-text")}>
-              Text record
-              <sl-icon slot="prefix" name="chat-right-text"></sl-icon>
-            </sl-menu-item>
-            <sl-menu-item @click=${() => addRecord("record-url")}>
-              URL record
-              <sl-icon slot="prefix" name="house"></sl-icon>
-            </sl-menu-item>
-            <sl-menu-item @click=${() => addRecord("record-absolute-url")}>
-              Absolute URL record
-              <sl-icon slot="prefix" name="house"></sl-icon>
-            </sl-menu-item>
-            <sl-menu-item @click=${() => addRecord("record-empty")}>
-              Smart Poster record
-              <sl-icon slot="prefix" name="easel2"></sl-icon>
-            </sl-menu-item>
-            <sl-menu-item @click=${() => addRecord("record-mime")}>
-              MIME type record
-              <sl-icon slot="prefix" name="file-bar-graph"></sl-icon>
-            </sl-menu-item>
-            <sl-menu-item @click=${() => addRecord("record-external")}>
-              External type record
-              <sl-icon slot="prefix" name="box-arrow-up-right"></sl-icon>
-            </sl-menu-item>
-            <sl-menu-item @click=${() => addRecord("record-empty")}>
-              Unknown record
-              <sl-icon slot="prefix" name="patch-question"></sl-icon>
-            </sl-menu-item>
-          </sl-menu>
-        </sl-dropdown>
-      </div>
+      <sl-dropdown>
+        <sl-button variant="default" slot="trigger" >
+          <sl-icon slot="prefix" name="plus-square-dotted"></sl-icon>
+          Add subrecord
+        </sl-button>
+        <sl-menu>
+          <sl-menu-item @click=${() => addRecord("record-empty")}>
+            Empty record
+            <sl-icon slot="prefix" name="chat-right"></sl-icon>
+          </sl-menu-item>
+          <sl-menu-item @click=${() => addRecord("record-text")}>
+            Text record
+            <sl-icon slot="prefix" name="chat-right-text"></sl-icon>
+          </sl-menu-item>
+          <sl-menu-item @click=${() => addRecord("record-url")}>
+            URL record
+            <sl-icon slot="prefix" name="house"></sl-icon>
+          </sl-menu-item>
+          <sl-menu-item @click=${() => addRecord("record-absolute-url")}>
+            Absolute URL record
+            <sl-icon slot="prefix" name="house"></sl-icon>
+          </sl-menu-item>
+          <sl-menu-item @click=${() => addRecord("record-empty")}>
+            Smart Poster record
+            <sl-icon slot="prefix" name="easel2"></sl-icon>
+          </sl-menu-item>
+          <sl-menu-item @click=${() => addRecord("record-mime")}>
+            MIME type record
+            <sl-icon slot="prefix" name="file-bar-graph"></sl-icon>
+          </sl-menu-item>
+          <sl-menu-item @click=${() => addRecord("record-external")}>
+            External type record
+            <sl-icon slot="prefix" name="box-arrow-up-right"></sl-icon>
+          </sl-menu-item>
+          <sl-menu-item @click=${() => addRecord("record-empty")}>
+            Unknown record
+            <sl-icon slot="prefix" name="patch-question"></sl-icon>
+          </sl-menu-item>
+        </sl-menu>
+      </sl-dropdown>
     </div>
     `;
   }
@@ -460,8 +516,8 @@ class NFCTools extends LitElement {
     align-content: stretch;
   }
 
-  hr {
-    width: 100%;
+  sl-button {
+    margin: 0 0 16px 0;
   }
   `;
 
@@ -470,12 +526,16 @@ class NFCTools extends LitElement {
   }
 
   write() {
-    const records = this.rootRecord.records();
-    console.table(records);
-    if ("NDEFWriter" in window) {
-      // Hook up with toast or some dialog
-      const writer = new NDEFWriter();
-      writer.write({ records });
+    try {
+      const records = this.rootRecord.records();
+      console.log(JSON.stringify(records, null, 2));
+      if ("NDEFWriter" in window) {
+        // Hook up with toast or some dialog
+        const writer = new NDEFWriter();
+        writer.write({ records });
+      }
+    } catch {
+      console.error("Write failed.");
     }
   }
 
